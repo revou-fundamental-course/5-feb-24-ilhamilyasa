cdb793e5c8452750cd9308cf2bba5f7b4d3f56ec
ilhamilyasa <ilhamilyasa2005@gmail.com>
2024-02-11 09:19:10 +0700

    firts push

---
+++ script.js
@@ -0,0 +1,20 @@
+function convertTemp() {
+    var tempInput = parseFloat(document.getElementById('tempInput').value);
+    var tempScale = document.getElementById('tempScale').value;
+    var result;
+    var calculationText;
+
+    if (tempScale === 'celsius') {
+        // Konversi dari Celsius ke Fahrenheit: (Celsius * 9/5) + 32
+        result = (tempInput * 9/5) + 32;
+        calculationText = tempInput + " Celsius * (9/5) + 32 = " + result + " Fahrenheit";
+        document.getElementById('result').innerHTML = tempInput + " Celsius = " + result + " Fahrenheit";
+    } else {
+        // Konversi dari Fahrenheit ke Celsius: (Fahrenheit - 32) * 5/9
+        result = (tempInput - 32) * 5/9;
+        calculationText = tempInput + " Fahrenheit - 32) * (5/9) = " + result + " Celsius";
+        document.getElementById('result').innerHTML = tempInput + " Fahrenheit = " + result + " Celsius";
+    }
+
+    document.getElementById('calculation').innerHTML = "Kalkulasi: " + calculationText;
+}
